83b3faf0ff1840cd05345783e32fce9a15a96113
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-09 03:45:21 +0700

    📦 Put some very basic usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use minigrep::Application;
+
 fn main() {
     println!("Hello, world!");
+
+    let app = Application::new();
+    if let Err(e) = app {
+        match e {
+            minigrep::ErrorKind::QueryEmpty => eprintln!("Query is empty"),
+            minigrep::ErrorKind::FilenameEmpty => eprintln!("Filename is empty"),
+            minigrep::ErrorKind::ParseArgs => eprintln!("Failed to parse Args"),
+            minigrep::ErrorKind::OpenFile(e) => eprintln!("Failed top open a file: {}", e),
+            minigrep::ErrorKind::NotEnoughArgs(len) => {
+                eprintln!("Not enough Args expected 2 given {}", len)
+            }
+        }
+    } else if let Ok(app) = app {
+        println!("{:#?}", app);
+    }
 }
