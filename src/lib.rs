0641f7188b670779e69e43909f454fda148801cc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-09 03:04:52 +0700

    📦 Laying down foundation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+use std::env;
+
+enum ErrorKind {
+    ParseArgs,
+    OpenFile,
+}
+
+struct Application<'a> {
+    config: Config<'a>,
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+    case_sensitive: bool,
+}
+
+struct Parser;
+
+impl<'a> Application<'a> {
+    fn new() -> Application<'a> {
+        todo!()
+    }
+
+    fn parse() -> Vec<&'a str> {
+        todo!()
+    }
+}
+
+impl<'a> Config<'a> {
+    fn new(filename: &'a str, query: &'a str, case_sensitive: bool) -> Config<'a> {
+        Config {
+            filename,
+            query,
+            case_sensitive,
+        }
+    }
+
+    fn parse() -> Result<Config<'a>, ErrorKind> {
+        let args = env::args();
+
+        if args.len() < 2 {
+            return Err(ErrorKind::ParseArgs);
+        }
+
+        //properly parse cmd args to make config struct
+        Ok(Config {
+            query: todo!(),
+            filename: todo!(),
+            case_sensitive: todo!(),
+        })
+    }
+}
+
+impl<'a> Parser {
+    fn parse(query: &str, filename: &'a str) -> Vec<&'a str> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod test {
     //
